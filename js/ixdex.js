90201976d19f11b61c6ac2826d2a9a5a31252fa4
PhengPenghak <95391236+PhengPenghak@users.noreply.github.com>
2023-06-17 16:34:38 +0700

    my calender

--- js/ixdex.js
+++ js/ixdex.js
@@ -3,4 +3,11 @@ const cardBody = card.querySelector('.card-body')
 
 card.addEventListener('click', () => {
   cardBody.classList.toggle('closed')
+});
+
+const cards = document.querySelector('.map-card');
+const cardBodys = cards.querySelector('.card-body')
+
+card.addEventListener('click', () => {
+  cardBody.classList.toggle('closed')
 })
